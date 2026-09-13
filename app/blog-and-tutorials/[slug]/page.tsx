6eb68d6f1504ba968b8891e064a3f769146eb503
Suvadip <suvadipsamanta321@gmail.com>
2026-09-13 17:18:55 +0530

    Added placeholders to empty files

---
+++ app/blog-and-tutorials/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function PlaceholderPage() {
+  return (
+    <div className="flex items-center justify-center min-h-[50vh] mt-8 fade-in">
+      <h1 className="text-3xl font-bold text-slate-300">
+        This Page is Currently Blank (TBD)
+      </h1>
+    </div>
+  );
+}
